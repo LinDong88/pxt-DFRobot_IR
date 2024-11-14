74019f5ce6b730b8ab7cf2caa98817673fac7fd3
李荣 <1595243018@qq.com>
2024-11-14 10:50:48 +0800

    更新 pxt.json, ir.ts

--- ir.ts
+++ ir.ts
@@ -1,65 +1,391 @@
+// MakerBit blocks supporting a Keyestudio Infrared Wireless Module Kit
+// (receiver module+remote controller)
 
-/**
- * Custom blocks
- */
-//% weight=100 color=#0fbc11 icon="\uf1eb" block="IR"
-namespace IR {
-    let state: number;
-    let data1: number;
-    let irstate: number;
-    let irData: number = -1;
 
-    //% shim=DFRobotIR::irCode
-    function irCode(): number {
-        return 0;
-    }
 
-    /**
-     * Read infrared value
-     */
-    
-    //% weight=60
-    //% block="read IR key value"
-    export function IR_read(): number {
-        pins.setPull(DigitalPin.P16, PinPullMode.PullUp)
-        return valuotokeyConversion();
-    }
+//% color=#0fbc11 icon="\u272a" block="MakerBit"
+//% category="MakerBit"
+namespace makerbit {
+    export namespace background {
 
-    /**
-     * The callback function when infrared information is received
-     */
+        export enum Thread {
+            Priority = 0,
+            UserCallback = 1,
+        }
 
-    //% weight=50
-    //% block="on IR received"
-    //% draggableParameters
-    export function IR_callbackUser(cb: (message: number) => void) {
-        pins.setPull(DigitalPin.P16, PinPullMode.PullUp)
-        state = 1;
-        control.onEvent(11, 22, function () {
-            cb(data1)
+        export enum Mode {
+            Repeat,
+            Once,
+        }
 
-        })
-    }
+        class Executor {
+            _newJobs: Job[] = undefined;
+            _jobsToRemove: number[] = undefined;
+            _pause: number = 100;
+            _type: Thread;
 
-    basic.forever(() => {
-        if (state == 1) {
-            irstate = irCode();
-            if (irstate != 0) {
-                data1 = irstate & 0xff;
-                control.raiseEvent(11, 22)
+            constructor(type: Thread) {
+                this._type = type;
+                this._newJobs = [];
+                this._jobsToRemove = [];
+                control.runInParallel(() => this.loop());
+            }
+
+            push(task: () => void, delay: number, mode: Mode): number {
+                if (delay > 0 && delay < this._pause && mode === Mode.Repeat) {
+                    this._pause = Math.floor(delay);
+                }
+                const job = new Job(task, delay, mode);
+                this._newJobs.push(job);
+                return job.id;
+            }
+
+            cancel(jobId: number) {
+                this._jobsToRemove.push(jobId);
+            }
+
+            loop(): void {
+                const _jobs: Job[] = [];
+
+                let previous = control.millis();
+
+                while (true) {
+                    const now = control.millis();
+                    const delta = now - previous;
+                    previous = now;
+
+                    // Add new jobs
+                    this._newJobs.forEach(function (job: Job, index: number) {
+                        _jobs.push(job);
+                    });
+                    this._newJobs = [];
+
+                    // Cancel jobs
+                    this._jobsToRemove.forEach(function (jobId: number, index: number) {
+                        for (let i = _jobs.length - 1; i >= 0; i--) {
+                            const job = _jobs[i];
+                            if (job.id == jobId) {
+                                _jobs.removeAt(i);
+                                break;
+                            }
+                        }
+                    });
+                    this._jobsToRemove = []
+
+
+                    // Execute all jobs
+                    if (this._type === Thread.Priority) {
+                        // newest first
+                        for (let i = _jobs.length - 1; i >= 0; i--) {
+                            if (_jobs[i].run(delta)) {
+                                this._jobsToRemove.push(_jobs[i].id)
+                            }
+                        }
+                    } else {
+                        // Execute in order of schedule
+                        for (let i = 0; i < _jobs.length; i++) {
+                            if (_jobs[i].run(delta)) {
+                                this._jobsToRemove.push(_jobs[i].id)
+                            }
+                        }
+                    }
+
+                    basic.pause(this._pause);
+                }
             }
         }
 
-        basic.pause(50);
-    })
+        class Job {
+            id: number;
+            func: () => void;
+            delay: number;
+            remaining: number;
+            mode: Mode;
 
-    function valuotokeyConversion(): number {
-        //serial.writeValue("x", irCode() )
-        let data = irCode();
-        if (data == 0) {
-        } else {
-            irData = data & 0xff;
+            constructor(func: () => void, delay: number, mode: Mode) {
+                this.id = randint(0, 2147483647)
+                this.func = func;
+                this.delay = delay;
+                this.remaining = delay;
+                this.mode = mode;
+            }
+
+            run(delta: number): boolean {
+                if (delta <= 0) {
+                    return false;
+                }
+
+                this.remaining -= delta;
+                if (this.remaining > 0) {
+                    return false;
+                }
+
+                switch (this.mode) {
+                    case Mode.Once:
+                        this.func();
+                        basic.pause(0);
+                        return true;
+                    case Mode.Repeat:
+                        this.func();
+                        this.remaining = this.delay;
+                        basic.pause(0);
+                        return false;
+                }
+            }
         }
-        return irData;
+
+        const queues: Executor[] = [];
+
+        export function schedule(
+            func: () => void,
+            type: Thread,
+            mode: Mode,
+            delay: number,
+        ): number {
+            if (!func || delay < 0) return 0;
+
+            if (!queues[type]) {
+                queues[type] = new Executor(type);
+            }
+
+            return queues[type].push(func, delay, mode);
+        }
+
+        export function remove(type: Thread, jobId: number): void {
+            if (queues[type]) {
+                queues[type].cancel(jobId);
+            }
+        }
     }
 }
+
+
+
+//% color=#0fbc11 icon="\u272a" block="MakerBit"
+//% category="MakerBit"
+namespace makerbit {
+    let irState: IrState;
+    const IR_REPEAT = 256;
+    const IR_INCOMPLETE = 257;
+    const IR_DATAGRAM = 258;
+    const REPEAT_TIMEOUT_MS = 120;
+    interface IrState {
+        hasNewDatagram: boolean;
+        bitsReceived: uint8;
+        addressSectionBits: uint16;
+        commandSectionBits: uint16;
+        hiword: uint16;
+        loword: uint16;
+        activeCommand: number;
+        repeatTimeout: number;
+        onIrDatagram: () => void;
+    }
+
+
+    function appendBitToDatagram(bit: number): number {
+        irState.bitsReceived += 1;
+
+        if (irState.bitsReceived <= 8) {
+            irState.hiword = (irState.hiword << 1) + bit;
+        } else if (irState.bitsReceived <= 16) {
+            irState.hiword = (irState.hiword << 1) + bit;
+        } else if (irState.bitsReceived <= 32) {
+            irState.loword = (irState.loword << 1) + bit;
+        }
+
+        if (irState.bitsReceived === 32) {
+            irState.addressSectionBits = irState.hiword & 0xffff;
+            irState.commandSectionBits = irState.loword & 0xffff;
+            return IR_DATAGRAM;
+        } else {
+            return IR_INCOMPLETE;
+        }
+    }
+
+    function decode(markAndSpace: number): number {
+        if (markAndSpace < 1600) {
+            // low bit
+            return appendBitToDatagram(0);
+        } else if (markAndSpace < 2700) {
+            // high bit
+            return appendBitToDatagram(1);
+        }
+
+        irState.bitsReceived = 0;
+
+        if (markAndSpace < 12500) {
+            // Repeat detected
+            return IR_REPEAT;
+        } else if (markAndSpace < 14500) {
+            // Start detected
+            return IR_INCOMPLETE;
+        } else {
+            return IR_INCOMPLETE;
+        }
+    }
+
+    function enableIrMarkSpaceDetection(pin: DigitalPin) {
+        pins.setPull(pin, PinPullMode.PullNone);
+
+        let mark = 0;
+        let space = 0;
+
+        pins.onPulsed(pin, PulseValue.Low, () => {
+            // HIGH, see https://github.com/microsoft/pxt-microbit/issues/1416
+            mark = pins.pulseDuration();
+        });
+
+        pins.onPulsed(pin, PulseValue.High, () => {
+            // LOW
+            space = pins.pulseDuration();
+            const status = decode(mark + space);
+
+            if (status !== IR_INCOMPLETE) {
+                handleIrEvent(status);
+            }
+        });
+    }
+
+
+    function handleIrEvent(irEvent: number) {
+
+        // Refresh repeat timer
+        if (irEvent === IR_DATAGRAM || irEvent === IR_REPEAT) {
+            irState.repeatTimeout = input.runningTime() + REPEAT_TIMEOUT_MS;
+        }
+
+        if (irEvent === IR_DATAGRAM) {
+            irState.hasNewDatagram = true;
+
+            if (irState.onIrDatagram) {
+                background.schedule(irState.onIrDatagram, background.Thread.UserCallback, background.Mode.Once, 0);
+            }
+
+            const newCommand = irState.commandSectionBits >> 8;
+
+        }
+    }
+
+    function initIrState() {
+        if (irState) {
+            return;
+        }
+
+        irState = {
+            bitsReceived: 0,
+            hasNewDatagram: false,
+            addressSectionBits: 0,
+            commandSectionBits: 0,
+            hiword: 0, // TODO replace with uint32
+            loword: 0,
+            activeCommand: -1,
+            repeatTimeout: 0,
+            onIrDatagram: undefined,
+        };
+    }
+
+    /**
+     * Connects to the IR receiver module at the specified pin and configures the IR protocol.
+     * @param pin IR receiver pin, eg: DigitalPin.P0
+     * @param protocol IR protocol, eg: IrProtocol.Keyestudio
+     */
+    //% subcategory="IR Receiver"
+    //% blockId="makerbit_infrared_connect_receiver"
+    //% block="connect IR receiver at pin %pin and decode %protocol"
+    //% pin.fieldEditor="gridpicker"
+    //% pin.fieldOptions.columns=4
+    //% pin.fieldOptions.tooltips="false"
+    //% weight=90
+    export function connectIrReceiver(): void {
+        initIrState();
+        enableIrMarkSpaceDetection(DigitalPin.P16)
+        background.schedule(notifyIrEvents, background.Thread.Priority, background.Mode.Repeat, REPEAT_TIMEOUT_MS);
+    }
+
+    function notifyIrEvents() {
+        if (irState.activeCommand === -1) {
+            // skip to save CPU cylces
+        } else {
+            const now = input.runningTime();
+            if (now > irState.repeatTimeout) {
+                // repeat timed out
+
+                // const handler = irState.onIrButtonReleased.find(h => h.irButton === irState.activeCommand || IrButton.Any === h.irButton);
+                // if (handler) {
+                //     background.schedule(handler.onEvent, background.Thread.UserCallback, background.Mode.Once, 0);
+                // }
+
+                irState.bitsReceived = 0;
+                irState.activeCommand = -1;
+            }
+        }
+    }
+
+    /**
+     * Do something when an IR datagram is received.
+     * @param handler body code to run when the event is raised
+     */
+    //% subcategory="IR Receiver"
+    //% blockId=makerbit_infrared_on_ir_datagram
+    //% block="on IR datagram received"
+    //% weight=40
+    export function onIrDatagram(handler: () => void) {
+        initIrState();
+        irState.onIrDatagram = handler;
+    }
+
+    /**
+     * Returns the IR datagram as 32-bit hexadecimal string.
+     * The last received datagram is returned or "0x00000000" if no data has been received yet.
+     */
+    //% subcategory="IR Receiver"
+    //% blockId=makerbit_infrared_ir_datagram
+    //% block="IR datagram"
+    //% weight=30
+    export function irDatagram(): string {
+        basic.pause(0); // Yield to support background processing when called in tight loops
+        initIrState();
+        return (
+            // "0x" +ir_rec_to16BitHex(irState.addressSectionBits) + ir_rec_to16BitHex(irState.commandSectionBits) 
+            ir_rec_to16BitHex(irState.commandSectionBits & 0x00ff)
+        );
+    }
+
+    /**
+     * Returns true if any IR data was received since the last call of this function. False otherwise.
+     */
+    //% subcategory="IR Receiver"
+    //% blockId=makerbit_infrared_was_any_ir_datagram_received
+    //% block="IR data was received"
+    //% weight=80
+    export function wasIrDataReceived(): boolean {
+        basic.pause(0); // Yield to support background processing when called in tight loops
+        initIrState();
+        if (irState.hasNewDatagram) {
+            irState.hasNewDatagram = false;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    function ir_rec_to16BitHex(value: number): string {
+        let hex = "";
+        for (let pos = 0; pos < 4; pos++) {
+            let remainder = value % 16;
+            if (remainder < 10) {
+                hex = remainder.toString() + hex;
+            } else {
+                hex = String.fromCharCode(55 + remainder) + hex;
+            }
+            value = Math.idiv(value, 16);
+        }
+        return hex;
+    }
+}
+
+
+
+
+
